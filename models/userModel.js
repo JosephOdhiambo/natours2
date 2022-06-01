59c31219deb2bccd05ccfc6e4c195abcbdadb7eb
modi <odhisjoseph85@gmail.com>
2022-06-01 12:58:09 +0300

    Managing Passwords || Install bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,10 +23,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your  password'],
-    minlength: [6, 'A password must have more than or = 6 characters']
+    validate: {
+      //This only works on create and save(mongoose)
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same'
+    }
   }
 });
+userSchema.pre('save', async function(next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
 
+  //Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 16);
+
+  //Delete the passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
