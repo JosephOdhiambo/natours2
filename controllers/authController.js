72ca11f2e31f85d6ec097c23f46e99bc54c5ee58
modi <odhisjoseph85@gmail.com>
2022-06-14 11:34:10 +0300

    New comment

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,7 +49,7 @@ exports.signup = catchAsync(async (req, res, next) => {
 });
 
 exports.login = catchAsync(async (req, res, next) => {
-  const { email, password } = req.body; // destructuring
+  const { email, password } = req.body; // destructuring reasonable doubt
   //1. Check if email and passwords exist
   if (!email || !password) {
     return next(new AppError('Please provide email and password', 400));
